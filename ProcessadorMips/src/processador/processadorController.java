156132ef841a5e289f98612dc1b5bd93b8db3537
Daniel Oliveira de Freitas <danisnake27@gmail.com>
2021-07-22 01:39:08 -0300

    add algumas coisas

--- ProcessadorMips/src/processador/processadorController.java
+++ ProcessadorMips/src/processador/processadorController.java
@@ -28,6 +28,29 @@ public class processadorController {
 		resultado.setText(String.valueOf(result));
 	}
 	
+	public void sub() {
+		int result,num1,num2;
+		num1 = Integer.parseInt(n1.getText());
+		num2 = Integer.parseInt(n2.getText());
+		result = num1-num2;
+		resultado.setText(String.valueOf(result));
+	}
+	
+	public void mult() {
+		int result,num1,num2;
+		num1 = Integer.parseInt(n1.getText());
+		num2 = Integer.parseInt(n2.getText());
+		result = num1*num2;
+		resultado.setText(String.valueOf(result));
+	}
+	
+	public void div() {
+		int result,num1,num2;
+		num1 = Integer.parseInt(n1.getText());
+		num2 = Integer.parseInt(n2.getText());
+		result = num1/num2;
+		resultado.setText(String.valueOf(result));
+	}
 	public void acaobotao() {
 		p.setVisible(true);
 		s.setVisible(true);
