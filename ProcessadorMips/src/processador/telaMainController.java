f5c7d34536f96495ddc9a631dee4a61346a92bbd
Daniel Oliveira de Freitas <danisnake27@gmail.com>
2021-08-19 15:43:25 -0300

    começando novas telas

---
+++ ProcessadorMips/src/processador/telaMainController.java
@@ -0,0 +1,32 @@
+package processador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class telaMainController {
+
+	   @FXML
+	    void mimps(ActionEvent event) {
+		   Stage primaryStage = new Stage();
+		   try {
+				AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("TelaProcessador.fxml"));
+				Scene scene = new Scene(root);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				primaryStage.setScene(scene);
+				primaryStage.show();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	
+	    @FXML
+	    void pipeline(ActionEvent event) {
+
+	    }
+
+	
+}
