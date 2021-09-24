a8f6e06f8639c1e15d710b0e890bc978de87fdbe
CassioJr <cassiojuniorceolin@gmail.com>
2021-09-24 17:31:35 -0300

    Alterações finais

--- ProcessadorMips/exe.bat
+++ ProcessadorMips/exe.bat
@@ -1,1 +1,5 @@
-java --module-path "C:\Apps\javafx-sdk-16\lib" --add-modules javafx.controls,javafx.fxml -jar "C:\Users\danie\Desktop\ArquiteturaeOrganizaçãodeComputadores\MipsSimulator\ProcessadorMips\exe.jar"
+#Instruções de como executar o programa 
+
+-- Primeiro você deve abrir seu terminal após isso cole o comando abaixo, ao lado do -jar aonde está escrito coloque o caminho do jar que esta dentro da pastaexecutavel:  
+
+java --module-path "C:\Apps\javafx-sdk-16\lib" --add-modules javafx.controls,javafx.fxml -jar c:/pastaExemplo
--- ProcessadorMips/src/controller/mCacheController.java
+++ ProcessadorMips/src/controller/mCacheController.java
@@ -13,18 +13,28 @@ public class mCacheController {
 	@FXML
 	private ImageView imageCache;
 	
-	
 	int count = 0;
-    public void botao() {
+    public void botaoProximo() {
                 if(count != 5) {
-                File file = new File("./imagens/memoria"+count+".png");
+                count++;
+                File file = new File("./Imagens/memoria"+count+".png");
                 Image image = new Image(file.toURI().toString());
                 imageCache.setImage(image);
-                count++;
+                System.out.println(count);
             }
             else if(count == 5) {
                 count = 0;
             }
         }
-
+    
+    public void botaoAnterior() {
+        if(count > 0) {
+        count--;
+        File file = new File("./Imagens/memoria"+count+".png");
+        Image image = new Image(file.toURI().toString());
+        imageCache.setImage(image);
+        System.out.println(count);
+    }
+}
+    
 }
